2c1a4aeb96578493c2177da66ccb3f8ff55b9c7c
sergio-ac <57115186+sergio-ac@users.noreply.github.com>
2020-05-20 14:06:34 -0500

    Botones de perfil en menu lateral

--- ArathsBaby-master/ArathsBaby/frontend/arathsbaby_app/lib/pages/home_page.dart
+++ ArathsBaby-master/ArathsBaby/frontend/arathsbaby_app/lib/pages/home_page.dart
@@ -1,19 +1,53 @@
 import 'package:flutter/material.dart';
 
 
-
 class HomePage extends StatefulWidget {
-  // This widget is the root of your application.
+    // This widget is the root of your application.
   @override
   _HomePageState createState() => new _HomePageState();
 }
 class _HomePageState extends State<HomePage>{
+  String mainProfilePicture = "https://cdn3.vectorstock.com/i/1000x1000/30/97/flat-business-man-user-profile-avatar-icon-vector-4333097.jpg";
+String otherProfilePicture ="https://cdn2.vectorstock.com/i/1000x1000/41/11/flat-business-woman-user-profile-avatar-icon-vector-4334111.jpg";
+
+void switchUser(){
+  String backupString = mainProfilePicture;
+  this.setState(() {
+    mainProfilePicture = otherProfilePicture;
+    otherProfilePicture = backupString;
+  });
+}
+  @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(title: new Text("Araths Baby App"), backgroundColor: Colors.pinkAccent,),
       drawer: new Drawer(
         child: new ListView(
           children: <Widget>[
+            new UserAccountsDrawerHeader(
+                            accountName: new Text ("Sergio Alejo"),
+                            accountEmail: new Text("s@gmail.com"),
+                            currentAccountPicture: new GestureDetector(
+                              onTap:()=> switchUser(),
+                              child: new CircleAvatar(
+                                backgroundImage: new NetworkImage (mainProfilePicture),
+                              ),
+                            ),
+                            otherAccountsPictures: <Widget>[
+                              new GestureDetector(
+                                onTap:()=>switchUser(),
+                                child: new CircleAvatar(
+                                  backgroundImage: new NetworkImage(otherProfilePicture), 
+                                  ),
+                              )
+                            ],
+                            decoration: new BoxDecoration(
+                              image : new DecorationImage(
+                                fit: BoxFit.fill,
+                                image: new NetworkImage("https://i.pinimg.com/originals/7b/40/40/7b4040ba378868e0594a1049ca4cae3b.jpg")
+                                )
+                            ),
+            ),
             new ListTile(
               title: new Text("First Page"),
               trailing: new Icon(Icons.arrow_upward),
