af5b59873919fe78a7523c863cf906060c91702f
sergio-ac <57115186+sergio-ac@users.noreply.github.com>
2020-05-22 14:18:02 -0500

    Corrigiendo y probando api

--- ArathsBaby-master/ArathsBaby/backend/ArathsBaby.WebAPI/Controllers/UsuarioController.cs
+++ ArathsBaby-master/ArathsBaby/backend/ArathsBaby.WebAPI/Controllers/UsuarioController.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using Newtonsoft.Json;
 namespace ArathsBaby.WebAPI.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     public class UsuarioController : ControllerBase
     {
-        [Route("api/prueba")]
         [HttpGet]
-        public IEnumerable<string> Get(){
-            return new string[]{"valor 1","valor 2"};
+        public String prueba(){
+            return "hola";
         }
+        public String despedida(){
+            return "adios";
+        }
+                
     }
 }
